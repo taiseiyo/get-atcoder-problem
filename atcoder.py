21275d5edfdca0806057be47f0e83cd722e774f9
taiseiyo <53172326+taiseiyo@users.noreply.github.com>
2020-05-30 15:41:21 +0900

    new

---
+++ atcoder.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import re
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+def generate_url(alphabet, num):
+    url = "https://atcoder.jp/contests/abc"+num+"/tasks/abc"+num\
+        + "_"+alphabet
+    req = urllib.request.Request(url)
+    html = urllib.request.urlopen(req)
+    html = html.read().decode("utf-8")
+    return html
+
+
+def get_problem(html):
+    target = BeautifulSoup(html, "lxml")
+    text = ""
+    for line in target.find_all(text=True):
+        if(line[1:3] == "力例"):
+            line = re.sub(r"力例 [0-9]", "力例 : ", line)
+        text = text + line
+
+    start = text.find("問題文")
+    end = text.find("Score")
+    print(text[start+3:end])
+
+
+def main():
+    num = input("Enter a number above 020 : ")
+    for alphabet in ["a", "b", "c", "d"]:
+        html = generate_url(alphabet, num)
+        get_problem(html)
+        print("change problem")
+
+
+main()
